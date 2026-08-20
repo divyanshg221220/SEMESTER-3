4d602b37c7f79186396f3bfa9cc72289dda0d3ef
divyanshg221220 <divyanshg221220@outlook.com>
2026-08-20 13:56:18 +0530

    Implement binary search algorithm with user input for array elements

--- C - ANALYSIS AND DESIGN OF ALGORITHM/5.c
+++ C - ANALYSIS AND DESIGN OF ALGORITHM/5.c
@@ -1,1 +1,49 @@
 // Implement the binary search using divide and conquer technique
+#include <stdio.h>
+int binarySearch(int arr[], int i, int j, int key)
+{
+    if (j >= i)
+    {
+        int mid =(i + j) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            return binarySearch(arr, i, mid - 1, key);
+        }
+        else
+        {
+            return binarySearch(arr, mid + 1, j, key);
+        }
+    }
+    else
+    {
+        return -1;
+    }
+}
+int main(int argc, char const *argv[])
+{
+    int n, key;
+    printf("Enter number of elements to insert in array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter [%d] element: ", i);
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the key to search: ");
+    scanf("%d", &key);
+    int result = binarySearch(arr, 0, n - 1, key);
+    if (result == -1)
+    {
+        printf("ELEMENT NOT FOUND\n");
+    }
+    else
+    {
+        printf("Element found at index: %d\n", result);
+    }
+    return 0;
+}
