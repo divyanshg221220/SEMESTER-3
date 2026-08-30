7578e909908628afe74006ac656795598a93e90e
divyanshg221220 <divyanshg221220@outlook.com>
2026-08-30 23:27:46 +0530

    Add UniversityStudentMain class for student data input and display

--- JAVA - OBJECT ORIENTED PROGRAMMING USING JAVA LAB/3.java
+++ JAVA - OBJECT ORIENTED PROGRAMMING USING JAVA LAB/3.java
@@ -3,3 +3,25 @@
 // 1. Make the Student Class part of University Package.
 // 2. Make a separate file for Main Class which is a part of default package and import university package in the same to input and print student data.
 // Hint: Use Access Modifiers and Packages.
+import java.util.Scanner;
+import University.UniversityStudent;
+class UniversityStudentMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UniversityStudent UniversityStudent = new UniversityStudent();
+        System.out.print("Enter Roll Number: ");
+        int rollNo = scanner.nextInt();
+        UniversityStudent.setRollNo(rollNo);
+        System.out.print("Enter Name: ");
+        String name = scanner.next();
+        UniversityStudent.setName(name);
+        System.out.print("Enter Institute Code: ");
+        String instituteCode = scanner.next();
+        UniversityStudent.setInstituteCode(instituteCode);
+        System.out.println("\nStudent Details:");
+        System.out.println("Roll Number: " + UniversityStudent.getRollNo());
+        System.out.println("Name: " + UniversityStudent.getName());
+        System.out.println("Institute Code: " + UniversityStudent.getInstituteCode());
+        scanner.close();
+    }
+}
