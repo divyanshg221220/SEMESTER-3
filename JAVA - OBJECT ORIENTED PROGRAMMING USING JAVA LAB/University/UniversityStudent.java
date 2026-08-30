da8f7de23bba0e2e47ccf589cea62ffd911070ff
divyanshg221220 <divyanshg221220@outlook.com>
2026-08-30 23:27:16 +0530

    Add UniversityStudent class for managing student details

---
+++ JAVA - OBJECT ORIENTED PROGRAMMING USING JAVA LAB/University/UniversityStudent.java
@@ -0,0 +1,24 @@
+package University;
+public class UniversityStudent {
+    private int rollNo;
+    private String name;
+    private String instituteCode;
+    public int getRollNo() {
+        return rollNo;
+    }
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getInstituteCode() {
+        return instituteCode;
+    }
+    public void setInstituteCode(String instituteCode) {
+        this.instituteCode = instituteCode;
+    }
+}
