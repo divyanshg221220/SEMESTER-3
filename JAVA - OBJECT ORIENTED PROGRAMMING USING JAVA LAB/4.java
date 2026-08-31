885e1cccd7209af1867521c01a9a7310b88d6951
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2026-08-31 14:02:51 +0530

    .

---
+++ JAVA - OBJECT ORIENTED PROGRAMMING USING JAVA LAB/4.java
@@ -0,0 +1,2 @@
+Write a program to count number of units produced from a manufacturing unit.
+Write a program to count number of units produced from a manufacturing unit. Hint: use static variables to count number of objects created.
