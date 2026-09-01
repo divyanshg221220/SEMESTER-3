0440cb40d3c58fb600df6097262daa2369848b37
divyanshg221220 <divyanshg221220@outlook.com>
2026-09-01 22:46:33 +0530

    Fix formatting of comments in 4.java for clarity

--- JAVA - OBJECT ORIENTED PROGRAMMING USING JAVA LAB/4.java
+++ JAVA - OBJECT ORIENTED PROGRAMMING USING JAVA LAB/4.java
@@ -1,2 +1,12 @@
 // Write a program to count number of units produced from a manufacturing unit.
 // Write a program to count number of units produced from a manufacturing unit. Hint: use static variables to count number of objects created.
+import java.util.Scanner;
+class ManufacturingUnit {
+    
+}
+class Car{
+    
+}
+class ManufacturingUnitMain {
+    
+}
