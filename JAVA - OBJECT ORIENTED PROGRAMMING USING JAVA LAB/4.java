aab93106a5e8e407006e3fd446e0a8e1e2c09b38
divyanshg221220 <divyanshg221220@outlook.com>
2026-09-04 14:14:29 +0530

    Implement manufacturing unit program to count produced car units

--- JAVA - OBJECT ORIENTED PROGRAMMING USING JAVA LAB/4.java
+++ JAVA - OBJECT ORIENTED PROGRAMMING USING JAVA LAB/4.java
@@ -1,12 +1,32 @@
 // Write a program to count number of units produced from a manufacturing unit.
 // Write a program to count number of units produced from a manufacturing unit. Hint: use static variables to count number of objects created.
 import java.util.Scanner;
-class ManufacturingUnit {
-    
-}
-class Car{
+class Car {
     static int objCount = 0;
+    Car() {
+        objCount++;
+    }
+    static int getObjCount() {
+        return objCount;
+    }
+}
+class ManufacturingUnit {
+    public void produceUnits(int count) {
+        for (int i = 0; i < count; i++) {
+            new Car();
+        }
+    }
 }
 class ManufacturingUnitMain {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ManufacturingUnit manufacturingUnit = new ManufacturingUnit();
+        System.out.print("Enter the number of units to produce: ");
+        int count = scanner.nextInt();
+        manufacturingUnit.produceUnits(count);
+        System.out.println("Total number of car produced: " + Car.getObjCount());
+        int noOfWheels = 4;
+        System.out.println("Total number of wheels produced: " + noOfWheels * Car.getObjCount());
+        scanner.close();
+    } 
 }
